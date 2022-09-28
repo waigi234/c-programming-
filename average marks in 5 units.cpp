0fcb6a792d2bf82d2beb4c081fdba289fefcaa7a
waigi234 <113586720+waigi234@users.noreply.github.com>
2022-09-28 23:26:50 +0300

    Update average marks in 5 units.cpp

--- average marks in 5 units.cpp
+++ average marks in 5 units.cpp
@@ -1,25 +1,32 @@
-// program to grade the avarage marks in 5 units
+// program to grade the average marks in 5 units
 #include <iostream>
 using namespace std;
-int main(){
-int math,eng,french,cre,sci,avg;
+int main()
+{
+Float math,eng,french,cre,sci;
+Float avg;
 cout<<"enter the marks of the 5 units"<<endl;
-cin>>math>>eng>>french>>cre;
+cin>>math>>eng>>french>>cre>>sci;
 avg=(math+eng+french+sci+cre)/5;
-if (avg>=70&&avg<=100){
-    cout<<"grade A";
+Cout<<"the average is "<<avg;
+if (avg>=70&&avg<=100)
+{
+    cout<<" grade A";
 }
-else if (avg>=60&&avg<=69){
-    cout<<"grade B";
+else if (avg>=60&&avg<=69)
+{
+    cout<<" grade B";
 }
-else if (avg>=50&&avg<=59){
-    cout<<"grade C";
+else if (avg>=50&&avg<=59)
+{
+    cout<<" grade C";
 }
-else if (avg>=40&&avg<=49){
-    cout<<"grade D";
+else if (avg>=40&&avg<=49)
+{
+    cout<<" grade D";
 }
 else{
-    cout<<"grade E";
+    cout<<" grade E";
 }
     return 0;
 }
